6853e9844e6f322017efa652acc66d562b1b1772
aamirmehboob <amirmehboob16@gmail.com>
2025-03-07 04:38:04 +0500

    update:"Added a sereilizable check"

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,25 +1,32 @@
-
-
 "use client";
 import { configureStore } from "@reduxjs/toolkit";
-import toDoSlice from  "../redux/slices/toDoSlice"
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // Uses local storage
 import { combineReducers } from "redux";
+import toDoSlice from "../redux/slices/toDoSlice";
+import authSlice from "./slices/authSlice";
 
 const persistConfig = {
   key: "root",
   storage,
+  whitelist: ["auth", "todos"], // Specify which reducers to persist
 };
 
 const rootReducer = combineReducers({
-  todos: toDoSlice, // Fix state key
+  todos: toDoSlice, 
+  auth: authSlice,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REGISTER"], // Ignore Persist Actions
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
